7e21e06fcf27843a008a68072ddeaca7be6db5cd
Koshak_Mine <saranddmit@gmail.com>
2022-04-20 23:15:18 +0300

    Starting work

---
+++ src/main/java/com/koshakmine/forestx/Forestx.java
@@ -0,0 +1,10 @@
+package com.koshakmine.forestx;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Forestx implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
