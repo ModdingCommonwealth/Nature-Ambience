a67dff121fdecc2bf3a39575e3951c6feb307186
Koshak_Mine <saranddmit@gmail.com>
2022-04-21 21:30:01 +0300

    Trying to add first berry bush

--- src/main/java/com/koshakmine/forestx/Forestx.java
+++ src/main/java/com/koshakmine/forestx/Forestx.java
@@ -1,7 +1,13 @@
 package com.koshakmine.forestx;
 
+import com.koshakmine.forestx.items.BerryItem;
 import com.koshakmine.forestx.items.FoodItem;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Material;
+import net.minecraft.block.SweetBerryBushBlock;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -10,15 +16,24 @@ import net.minecraft.util.registry.Registry;
 
 public class Forestx implements ModInitializer {
     static String MOD_ID = "forestx";
+    //Items
     public static final Item Berrypie = new FoodItem(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(6).saturationModifier(6f).build()));
-    public static final Item Raspberry = new FoodItem(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(2).saturationModifier(1f).build()));
-    public static final Item Blackberry = new FoodItem(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(2).saturationModifier(1f).build()));
-    public static final Item Cranberry = new FoodItem(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(2).saturationModifier(1f).build()));
+    public static final Item Raspberry = new BerryItem(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(2).saturationModifier(1f).build()));
+    public static final Item Blackberry = new BerryItem(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(2).saturationModifier(1f).build()));
+    public static final Item Cranberry = new BerryItem(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(2).saturationModifier(1f).build()));
+    public static final Item Blueberry = new BerryItem(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(2).saturationModifier(1f).build()));
+    //Blocks
+    public static final SweetBerryBushBlock RaspberryBush1 = new SweetBerryBushBlock(FabricBlockSettings.of(Material.PLANT));
+    //Registry
     @Override
     public void onInitialize() {
+        //Items
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "berry_pie"), Berrypie);
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "raspberry"), Raspberry);
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "blackberry"), Blackberry);
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "cranberry"), Cranberry);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "blueberry"), Blueberry);
+        //Blocks
+        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "bush_raspberry1"), RaspberryBush1);
     }
 }
--- src/main/java/com/koshakmine/forestx/client/ForestxClient.java
+++ src/main/java/com/koshakmine/forestx/client/ForestxClient.java
@@ -1,13 +1,16 @@
 package com.koshakmine.forestx.client;
 
+import com.koshakmine.forestx.Forestx;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 @Environment(EnvType.CLIENT)
 public class ForestxClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(Forestx.RaspberryBush1, RenderLayer.getCutout());
     }
 }
